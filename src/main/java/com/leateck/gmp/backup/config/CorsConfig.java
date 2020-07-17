818269f551de9b13c637439ee6238ca5719daf8d
yangqianlu <1160713762@qq.com>
2020-07-17 15:14:12 +0800

    feat(CorsConfig): 添加跨域

---
+++ src/main/java/com/leateck/gmp/backup/config/CorsConfig.java
@@ -0,0 +1,44 @@
+package com.leateck.gmp.backup.config;
+
+import com.google.common.net.HttpHeaders;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * <p>Title: CorsConfig</p>
+ * <p>Description: </p>
+ * <p>@Copyright: Shanghai 2020</p>
+ *
+ * @author luyangqian
+ * version 1.0
+ * <pre>History
+ *          2020-07-17   luyangqian  Created
+ * </pre>
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").
+                        allowedOrigins("*"). //允许跨域的域名，可以用*表示允许任何域名使用
+                        allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS"). //允许任何方法（post、get等）
+                        allowedHeaders("*"). //允许任何请求头
+                        exposedHeaders("access-control-allow-headers",
+                        "access-control-allow-methods",
+                        "access-control-allow-origin",
+                        "access-control-max-age",
+                        "X-Frame-Options").
+                        allowCredentials(true). //带上cookie信息
+                        /*exposedHeaders(HttpHeaders.SET_COOKIE). */
+                        maxAge(3600L); //maxAge(3600)表明在3600秒内，不需要再发送预检验请求，可以缓存该结果
+            }
+        };
+    }
+}
