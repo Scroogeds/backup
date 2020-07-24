1de9f515eb3a0a77f6b7a1b9b4d7b51ae13c8c89
yangqianlu <1160713762@qq.com>
2020-07-24 16:10:34 +0800

    feat(Backup*): 完善代码-2

--- src/main/java/com/leateck/gmp/backup/config/InitJavaCronTypeConfigRunner.java
+++ src/main/java/com/leateck/gmp/backup/config/InitJavaCronTypeConfigRunner.java
@@ -31,6 +31,6 @@ public class InitJavaCronTypeConfigRunner implements IBackupApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        //backupService.initEnableCron();
+        backupService.initEnableCron();
     }
 }
